a71b6bc87bb6a643297b9a5f37d88a9cf1e92556
Esaurock <esaurock3@gmail.com>
2022-07-22 21:58:24 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { listenerCount } = require('process');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get('/',(req, res)=>{
+    res.send('Hola Esau Calle');
+})
+
+app.get('/products',(req, res)=>{
+    res.send('Aqui estan los productos');
+})
+app.get('/people',(req, res)=>{
+    res.send('Aqui estan los clientes');
+})
+
+app.listen(PORT,()=>{
+    console.log("LISTO!!!");
+})
